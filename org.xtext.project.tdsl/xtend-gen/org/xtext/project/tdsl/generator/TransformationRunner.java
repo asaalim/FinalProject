4d4f039bda459fd832195999f0a28630a673fa5a
asaalim <ayk271981@hotmail.com>
2015-07-13 20:52:55 +0100

    Transformation runner

--- org.xtext.project.tdsl/xtend-gen/org/xtext/project/tdsl/generator/TransformationRunner.java
+++ org.xtext.project.tdsl/xtend-gen/org/xtext/project/tdsl/generator/TransformationRunner.java
@@ -6,6 +6,15 @@ import org.xtext.project.tdsl.transfoDsl.Transformation;
 @SuppressWarnings("all")
 public class TransformationRunner {
   public String run(final Transformation t) {
-    return InputOutput.<String>println("My simple run function.");
+    String _xblockexpression = null;
+    {
+      InputOutput.<String>println("My simple run function.");
+      Class<? extends Transformation> _class = t.getClass();
+      Package _package = _class.getPackage();
+      Class<? extends Package> _class_1 = _package.getClass();
+      String _typeName = _class_1.getTypeName();
+      _xblockexpression = InputOutput.<String>println(_typeName);
+    }
+    return _xblockexpression;
   }
 }
